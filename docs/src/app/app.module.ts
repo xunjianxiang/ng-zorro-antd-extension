565589d82595d32d0da22442bd21d44a0debce5d
xunjianxiang <xunjianxiang@gmail.com>
2021-03-30 18:41:37 +0800

    build: 项目初始化

---
+++ docs/src/app/app.module.ts
@@ -0,0 +1,33 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { NZ_I18N } from 'ng-zorro-antd/i18n';
+import { zh_CN } from 'ng-zorro-antd/i18n';
+import { registerLocaleData } from '@angular/common';
+import zh from '@angular/common/locales/zh';
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { TableModule } from 'ng-zorro-antd-extension';
+
+registerLocaleData(zh);
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+    BrowserAnimationsModule,
+    FormsModule,
+    HttpClientModule,
+    AppRoutingModule,
+    TableModule,
+  ],
+  providers: [{ provide: NZ_I18N, useValue: zh_CN }],
+  bootstrap: [
+    AppComponent,
+  ]
+})
+export class AppModule { }
---
+++ library/src/public-api.ts
@@ -0,0 +1,6 @@
+/*
+ * Public API Surface of library
+ */
+
+export * from './table/table.component';
+export * from './table/table.module';
